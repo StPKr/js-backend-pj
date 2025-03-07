4d4566d9945fa6b142bcebd3c8ef3790a4af71e7
StPKr <sotfuni25@gmail.com>
2025-03-08 00:42:13 +0200

    MongoDB setrup

--- config/env.js
+++ config/env.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` }); //*if no prod environment is set goes to dev by default
 
-export const { PORT, NODE_EVN } = process.env;
+export const { PORT, NODE_ENV, DB_URI } = process.env;
