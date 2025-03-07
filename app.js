b3079f4c491423ccb61eb13154e2a8f8f2d04fb8
StPKr <sotfuni25@gmail.com>
2025-03-08 01:18:43 +0200

    error middleware

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongoDB.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
+
+
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the subscription tracker API!');
