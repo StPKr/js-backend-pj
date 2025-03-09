e3976a8beef2188b6b7857ee16dcc9f0ab4b106c
StPKr <sotfuni25@gmail.com>
2025-03-09 22:18:31 +0200

    sign in function

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -25,7 +25,7 @@ export const signUp = async (req, res, next) => {
 
         const newUsers = await User.create([{ name, email, password: hashedPassword }], { session });
 
-        const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN});
+        const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
 
         await session.commitTransaction();
         session.endSession();
@@ -47,7 +47,38 @@ export const signUp = async (req, res, next) => {
 }
 
 export const singnIn = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
 
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            const error = new Error('User not found');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            const error = new Error('Invalid password');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+
+        res.status(200).json({
+            success: true,
+            message: 'User signed in successfully',
+            data: {
+                token,
+                user,
+            }
+        })
+    } catch (error) {
+        next(error);
+    }
 }
 
 export const signOut = async (req, res, next) => {
