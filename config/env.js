912462a85be47c8556632dbb981a7e98a854543e
StPKr <sotfuni25@gmail.com>
2025-03-13 15:16:24 +0200

    bot and spam protection setup via Arcjet

--- config/env.js
+++ config/env.js
@@ -2,9 +2,10 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` }); //*if no prod environment is set goes to dev by default
 
-export const { 
-    PORT, 
-    NODE_ENV, 
+export const {
+    PORT,
+    NODE_ENV,
     DB_URI,
-    JWT_SECRET, JWT_EXPIRES_IN
+    JWT_SECRET, JWT_EXPIRES_IN,
+    ARCJET_ENV, ARCJET_KEY,
 } = process.env;
